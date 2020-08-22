56bbd1282b61e4edb316cb9410b80e5b32dae581
ncchs294 <70068289+ncchs294@users.noreply.github.com>
2020-08-23 02:16:31 +0900

    Create bak_liff.js

---
+++ bak_liff.js
@@ -0,0 +1,106 @@
+$(document).ready(function () {
+    // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
+    // LINE DevelopersのLIFF画面より確認可能
+    var liffId = "1654689972-a3rzGmrE";
+    initializeLiff(liffId);
+    
+    
+})
+
+function initializeLiff(liffId) {
+    liff
+        .init({
+            liffId: liffId
+        })
+        .then(() => {
+            // Webブラウザからアクセスされた場合は、LINEにログインする
+            if (!liff.isInClient() && !liff.isLoggedIn()) {
+                window.alert("LINEアカウントにログインしてください。");
+                liff.login({redirectUri: location.href});
+            }
+        })
+        .catch((err) => {
+            window.alert('LIFF Initialization failed ', err);
+        });
+}
+
+function sendText(text) {
+    if (!liff.isInClient()) {
+        shareTargetPicker(text);
+    } else {
+        sendMessages(text);
+    }
+}
+
+// LINEトーク画面上でメッセージ送信
+
+function sendMessages(text) {
+    liff.sendMessages([{
+  "type": "flex",
+  "altText": "Flex Message",
+  "contents": {
+    "type": "bubble",
+    "direction": "ltr",
+    "header": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": "メモを登録しました",
+          "size": "lg",
+          "align": "center",
+          "weight": "bold",
+          "color": "#340AD9"
+        },
+        {
+          "type": "separator"
+        }
+      ]
+    },
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "contents": [
+        {
+          "type": "text",
+          "text": text,
+          "size": "lg",
+          "align": "start",
+          "gravity": "top",
+          "color": "#BF0202",
+          "wrap": true
+        },
+        {
+          "type": "separator"
+        }
+      ]
+    }
+  }
+}]).then(function () {
+        //liff.closeWindow();
+    }).catch(function (error) {
+        window.alert('Failed to send message ' + error);
+    });
+}
+/*
+function sendMessages(text) {
+    liff.sendMessages([{
+        'type': 'text',
+        'text': text
+    }]).then(function () {
+        liff.closeWindow();
+    }).catch(function (error) {
+        window.alert('Failed to send message ' + error);
+    });
+}
+*/
+// Webブラウザからメッセージ送信
+function shareTargetPicker(text) {
+    liff.shareTargetPicker([{
+        'type': 'text',
+        'text': text
+    }]).catch(function (error) {
+        window.alert('Failed to send message ' + error);
+    });
+}
