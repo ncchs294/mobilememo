265287096680f1148e24eb5d58e3f568e8a7d936
ncchs294 <70068289+ncchs294@users.noreply.github.com>
2020-08-24 20:18:22 +0900

    Update and rename bak20200823_index.js to bak20200824_index.js

--- bak20200824_index.js
+++ bak20200824_index.js
@@ -55,10 +55,19 @@ $(function () {
 
      //alert(JSON.stringify(JSONdata));
      // {"cd":"100", "name":"Taro"},
+	      
+     //スピナー表示
+     //インジケータ表示
+     // Loading 画像を表示
+     dispLoading("処理中...");
+	      
      $.post(url,
       JSONdata,
       function(dt){
          //console.log(dt);liff.closeWindow();
+	          //インジケータ除去
+	          // Loading 画像を消す
+                  removeLoading();
 	          if(dt.message == 'success!')
 		  {
 			  sendText(inputdata);//To LINE 送信
