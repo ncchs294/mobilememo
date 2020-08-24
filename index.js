f3c322743951094fd278c0ad47831b086ee531a4
ncchs294 <70068289+ncchs294@users.noreply.github.com>
2020-08-24 15:23:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -55,10 +55,21 @@ $(function () {
 
      //alert(JSON.stringify(JSONdata));
      // {"cd":"100", "name":"Taro"},
+	      
+     //スピナー表示
+     //インジケータ表示
+     var indicator = this.indicator({
+                     message: '処理中'
+                     }).show();
+	      
+	      
+	      
      $.post(url,
       JSONdata,
       function(dt){
          //console.log(dt);liff.closeWindow();
+	          //インジケータ除去
+                  indicator.hide();
 	          if(dt.message == 'success!')
 		  {
 			  sendText(inputdata);//To LINE 送信
